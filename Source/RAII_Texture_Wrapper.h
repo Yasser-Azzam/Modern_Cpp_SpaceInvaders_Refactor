52f9e8b70af3102882bee2d2b546a408ec9eb312
Yasser <yasser.azzam.0346@student.uu.se>
2026-01-13 19:44:41 +0100

    nothing important just made TextureWrapper code easier for my eyes to look at

--- Source/RAII_Texture_Wrapper.h
+++ Source/RAII_Texture_Wrapper.h
@@ -8,16 +8,17 @@ class TextureWrapper {
 public:
     TextureWrapper() = default;
 
-    explicit TextureWrapper(const char* path)
-        : texture_(LoadTexture(path))
+    explicit TextureWrapper(const char* path) : texture_(LoadTexture(path))
     {
-        if (texture_.id == 0) {
+        if (texture_.id == 0) 
+        {
             throw std::runtime_error("Failed to load texture");
         }
     }
 
     ~TextureWrapper() {
-        if (texture_.id != 0) {
+        if (texture_.id != 0) 
+        {
             UnloadTexture(texture_);
         }
     }
@@ -25,21 +26,28 @@ public:
     TextureWrapper(const TextureWrapper&) = delete;
     TextureWrapper& operator=(const TextureWrapper&) = delete;
 
-    TextureWrapper(TextureWrapper&& other) noexcept
-        : texture_(std::exchange(other.texture_, {})) {
+    TextureWrapper(TextureWrapper&& other) noexcept  : texture_(std::exchange(other.texture_, {})) 
+    {
+
     }
 
-    TextureWrapper& operator=(TextureWrapper&& other) noexcept {
-        if (this != &other) {
-            if (texture_.id != 0) {
+    TextureWrapper& operator=(TextureWrapper&& other) noexcept 
+    {
+        if (this != &other) 
+        {
+            if (texture_.id != 0) 
+            {
                 UnloadTexture(texture_);
             }
+
             texture_ = std::exchange(other.texture_, {});
         }
+
         return *this;
     }
 
-    [[nodiscard]] const Texture2D& get() const noexcept {
+    [[nodiscard]] const Texture2D& get() const noexcept 
+    {
         return texture_;
     }
 
